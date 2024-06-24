9236ee981134be5217ffab197fd1b67cce240f74
Gregory Poole <gbpoole@gmail.com>
2024-06-24 13:24:00 +1000

    More robust implementation of documentation update.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,10 +2,21 @@ import fileinput
 import os
 import subprocess
 import tomlkit
+from typing import List
 
 # Rich is a cookiecutter dependency, so these should be safe here
 from rich.console import Console
 from rich.markdown import Markdown
+
+
+class DocsUpdateException(Exception):
+    """Exception raised when an error is encountered during a documentation update."""
+
+    def __init__(self, message: str):
+        self._message = message
+
+    def __str__(self) -> str:
+        return f"{self._message}"
 
 
 def update_pyproject_toml() -> None:
@@ -42,42 +53,70 @@ def add_entry_to_docs_toc(entry: str) -> None:
     def indent_entry(indent:int) -> str:
         return " "*indent + entry
 
+    def write_toc(buffer: List) -> None:
+
+        # Find the last non-empty line and determine the indent being used
+        last_line_index=0
+        indent = -1
+        for i_line,line in enumerate(buffer):
+            if len(line.strip())>0:
+                last_line_index=i_line
+                # Determine the indent
+                line_indent = len(line) - len(line.lstrip())
+                if indent < 0:
+                    if line_indent > 0:
+                        indent = line_indent
+                elif indent!=line_indent:
+                    DocsUpdateException(f"Indentation error at TOC line {i_line}: {line_indent} spaces instead of {indent}")
+
+        # Sanity checks
+        if indent <=0:
+            DocsUpdateException(f"Indentation error in TOC: indent of {indent} determined.")
+
+        # Insert new line into buffer, after last non-empty line
+        buffer[last_line_index] = buffer[last_line_index] + indent_entry(indent) + os.linesep
+
+        # Write the buffer
+        for line in buffer:
+            print(line, end='')
+
     # Re-write the index.rst file with the given line added
-    with fileinput.input(files="{{cookiecutter | repo_path}}/docs/index.rst", inplace=True) as file:
-        flag_toc_found = False
-        flag_options_done = False
+    filename="{{cookiecutter | repo_path}}/docs/index.rst"
+    with fileinput.input(files=filename, inplace=True) as file:
+        flag_processing_toc = False
         flag_update_done = False
         indent = 0
+        buffer = []
         for i_line, line_in in enumerate(file):
             line_in_strip = line_in.strip()
             line_in_lstrip = line_in.lstrip(' ')
             line_in_length = len(line_in_strip)
             line_in_indent = len(line_in) - len(line_in_lstrip)
             line_out = line_in # re-write line unchanged by default
-            if line_in_length>0 and not flag_update_done:
-                # Don't modify until we find the TOC block
-                if not flag_toc_found:
-                    if line_in_strip.startswith(".. toctree"):
-                        flag_toc_found = True
-                # ... once we have, don't modify until after the end of the parameter block ...
-                elif not flag_options_done:
-                    if not indent:
-                        indent = line_in_indent
-                    elif indent != line_in_indent:
-                        raise Exception(f"Indentation error at line {i_line}: {line_in_indent} spaces instead of {indent}; line_length={line_in_length}.")
-                    flag_line_is_option = line_in_strip.startswith(":")
-                    if not flag_line_is_option:
-                        flag_options_done = True
-                # ... don't modify anything before the end of the parameter block or after we have written the new entry
-                if flag_options_done and not flag_update_done:
-                    if line_in_indent==0:
-                        line_out = indent_entry(indent) + os.linesep + line_in
+            if line_in_length>0:
+                if line_in_strip.startswith(".."):
+                    if line_in_strip.lstrip(". ").startswith("toctree"):
+                        flag_processing_toc = True
+                    else:
+                        # If we were reading the toc but aren't now, write the modified toc
+                        if flag_processing_toc:
+                            if flag_update_done:
+                                DocsUpdateException(f"Multiple insert points found in '{filename}.'")
+                            write_toc(buffer)
+                            flag_update_done = True
+                        flag_processing_toc = False
+                elif line_in_indent==0:
+                    # If we were reading the toc but aren't now, write the modified toc
+                    if flag_processing_toc:
+                        if flag_update_done:
+                            DocsUpdateException(f"Multiple insert points found in '{filename}.'")
+                        write_toc(buffer)
                         flag_update_done = True
-            elif flag_options_done and not flag_update_done:
-                line_out = indent_entry(indent) + os.linesep + line_in
-                flag_update_done = True
-
-            print(line_out, end='')
+                    flag_processing_toc = False
+            if flag_processing_toc:
+                buffer.append(line_in)
+            else:
+                print(line_out, end='')
 
 
 def print_instructions() -> None:
